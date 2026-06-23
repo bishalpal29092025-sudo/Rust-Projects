d01ca7cccedbcd894e547766a39c0c3e112c6083
Bishal Pal <bishalpal29092025@gmail.com>
2026-06-23 06:06:17 +0530

    Remove restaurant-house project

--- Class Projects/restaurant-house/target/debug/deps/restaurant_house-a42135c1e37ab29a.d
+++
@@ -1,5 +0,0 @@
-/Users/bishalpal/Desktop/Rust-Projects/Class Projects/restaurant-house/target/debug/deps/restaurant_house-a42135c1e37ab29a.d: src/main.rs
-
-/Users/bishalpal/Desktop/Rust-Projects/Class Projects/restaurant-house/target/debug/deps/librestaurant_house-a42135c1e37ab29a.rmeta: src/main.rs
-
-src/main.rs:
--- Class Projects/restaurant-house/target/debug/restaurant-house.d
+++
@@ -1,1 +0,0 @@
-/Users/bishalpal/Desktop/Rust-Projects/Class\ Projects/restaurant-house/target/debug/restaurant-house: /Users/bishalpal/Desktop/Rust-Projects/Class\ Projects/restaurant-house/src/main.rs
