6f0fb1ac47ec4350de1084cfbb21c0b21a18ce44
Bishal Pal <bishalpal29092025@gmail.com>
2026-06-27 23:59:27 +0530

    Remove target directory from version control

--- banking-system/target/debug/banking-system.d
+++
@@ -1,1 +0,0 @@
-/Users/bishalpal/Desktop/Rust-Projects/banking-system/target/debug/banking-system: /Users/bishalpal/Desktop/Rust-Projects/banking-system/src/main.rs
--- banking-system/target/debug/deps/banking_system-84556cd85daf6a83.d
+++
@@ -1,5 +0,0 @@
-/Users/bishalpal/Desktop/Rust-Projects/banking-system/target/debug/deps/banking_system-84556cd85daf6a83.d: src/main.rs
-
-/Users/bishalpal/Desktop/Rust-Projects/banking-system/target/debug/deps/libbanking_system-84556cd85daf6a83.rmeta: src/main.rs
-
-src/main.rs:
